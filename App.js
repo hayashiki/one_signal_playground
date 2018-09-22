a2a79982419d9cf1d05ffd486a7a3f1c3b23fa17
hayashiki <peperoncino.pop@gmail.com>
2018-09-22 09:52:55 +0900

    Handle env

--- App.js
+++ App.js
@@ -1,13 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
+import OneSignal from 'react-native-onesignal'; 
 import { StyleSheet, Text, View } from 'react-native';
+// import { ONESIGNAL_APP_ID } from 'react-native-dotenv';
 
-export default class App extends React.Component {
+export default class App extends Component {
+
+  componentWillMount() {
+      OneSignal.init(process.env.ONESIGNAL_APP_ID);    
+      OneSignal.addEventListener('received', this.onReceived);
+      OneSignal.addEventListener('opened', this.onOpened);
+      OneSignal.addEventListener('ids', this.onIds);
+  }
+
+  componentWillUnmount() {
+      OneSignal.removeEventListener('received', this.onReceived);
+      OneSignal.removeEventListener('opened', this.onOpened);
+      OneSignal.removeEventListener('ids', this.onIds);
+  }
+
+  onReceived(notification) {
+      console.log("Notification received: ", notification);
+  }
+
+  onOpened(openResult) {
+    console.log('Message: ', openResult.notification.payload.body);
+    console.log('Data: ', openResult.notification.payload.additionalData);
+    console.log('isActive: ', openResult.notification.isAppInFocus);
+    console.log('openResult: ', openResult);
+  }
+
+  onIds(device) {
+  console.log('Device info: ', device);
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text>one signal app!</Text>
       </View>
     );
   }
