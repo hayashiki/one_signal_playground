095fb9b3a9d139c22ac5d519b19fc9a33f8ae4fb
hayashiki <peperoncino.pop@gmail.com>
2018-09-26 18:39:09 +0900

    Impliment Get PlayerId

--- App.js
+++ App.js
@@ -1,42 +1,73 @@
 import React, { Component } from 'react';
 import OneSignal from 'react-native-onesignal'; 
-import { StyleSheet, Text, View } from 'react-native';
-// import { ONESIGNAL_APP_ID } from 'react-native-dotenv';
+import { StyleSheet, Text, View, TouchableHighlight } from 'react-native';
+import { ONESIGNAL_APP_ID } from 'react-native-dotenv'
+import axios from 'axios';
 
 export default class App extends Component {
 
-  componentWillMount() {
-      OneSignal.init(process.env.ONESIGNAL_APP_ID);    
-      OneSignal.addEventListener('received', this.onReceived);
-      OneSignal.addEventListener('opened', this.onOpened);
-      OneSignal.addEventListener('ids', this.onIds);
+  constructor (props) {
+    super()
+    this.state = {
+      player_id: 'aa',
+    }
+
+    // OneSignal.setLogLevel(7, 0);
+    OneSignal.init(ONESIGNAL_APP_ID)
+    // , {
+    //   kOSSettingsKeyAutoPrompt: true
+    // });
+    // OneSignal.setLocationShared(true);
+    OneSignal.addEventListener('ids', this.onIds);
+    OneSignal.addEventListener('received', this.onReceived);
+    OneSignal.addEventListener('opened', this.onOpened);
+    OneSignal.configure();
   }
+
+  // componentWillMount() {
+  //   OneSignal.init(ONESIGNAL_APP_ID)
+  //   OneSignal.addEventListener('received', this.onReceived);
+  //   OneSignal.addEventListener('opened', this.onOpened);
+  // }
 
   componentWillUnmount() {
-      OneSignal.removeEventListener('received', this.onReceived);
-      OneSignal.removeEventListener('opened', this.onOpened);
-      OneSignal.removeEventListener('ids', this.onIds);
+    OneSignal.removeEventListener('received', this.onReceived);
+    OneSignal.removeEventListener('opened', this.onOpened);
+    OneSignal.removeEventListener('ids', this.onIds);
   }
 
-  onReceived(notification) {
-      console.log("Notification received: ", notification);
+  onReceived = notification => {
+    console.log("Notification received: ", notification);
   }
 
-  onOpened(openResult) {
+  onOpened = openResult => {
     console.log('Message: ', openResult.notification.payload.body);
     console.log('Data: ', openResult.notification.payload.additionalData);
     console.log('isActive: ', openResult.notification.isAppInFocus);
     console.log('openResult: ', openResult);
   }
 
-  onIds(device) {
-  console.log('Device info: ', device);
+  onIds = device => {
+    this.setState({
+      player_id: device.userId
+    })
+  }
+
+  handleRegistDevice = () => {
+    const { player_id } = this.state;
+    axios.get('https://7c122e7c.ngrok.io?player_id=' + player_id)
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <Text>one signal app!</Text>
+        
+        <TouchableHighlight
+          
+          onPress={this.handleRegistDevice}
+        >
+          <Text>one signal app!</Text>
+        </TouchableHighlight>
       </View>
     );
   }
